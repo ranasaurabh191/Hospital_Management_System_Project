7c81fb502f1c3e7f26df66615fd354db5188dbb6
Saurabh Rana <ranasaurabh191@gmail.com>
2026-03-01 13:31:43 +0530

    Update

---
+++ Project/DoctorPatientManagementSystem/DoctorPatientManagementSystem/Interfaces/IDoctorManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoctorPatientManagementSystem.Interfaces
+{
+    public interface IDoctorManager
+    {
+        void AddDoctor();
+        void ListDoctors();
+        void DeleteDoctor();
+        // DB operations
+        void AddDoctorToDB();
+        void ListDoctorsFromDB();
+        void DeleteDoctorFromDB();
+    }
+}
